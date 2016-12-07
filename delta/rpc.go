f9d593ed729fa10d1f6b90dc8faa1e317c1bcd73
Andrew Martin <a.a.d.martin@gmail.com>
2016-12-07 23:55:13 +1100

    Added server url via env var

--- delta/rpc.go
+++ delta/rpc.go
@@ -3,12 +3,18 @@ package delta
 import (
 	"github.com/valyala/gorpc"
 	"time"
+	"os"
 )
 
 func NewRPClient() *gorpc.Client {
 
+	server_url := os.Getenv("SERVER")
+        if server_url == "" {
+                server_url = "127.0.0.1:12345"
+        }	
+
 	c := &gorpc.Client{
-		Addr:           "127.0.0.1:12345",
+		Addr:           server_url,
 		RequestTimeout: 90000 * time.Second,
 	}
 	c.Start()
